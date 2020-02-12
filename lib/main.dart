f192f11616745be66f74691cc23f5d047730a8a0
drmilde <milde@hs-fulda.de>
2020-02-12 09:35:58 +0100

    ReorderbaleExample eingebaut

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:einkaufsliste/screens/main_reordable_screen.dart';
 import 'package:einkaufsliste/screens/main_screen.dart';
 import 'package:flutter/material.dart';
 
@@ -12,7 +13,8 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      home: MainScreen("Meine Einkaufsliste"),
+      //home: MainScreen("Meine Einkaufsliste"),
+      home: MainReordableScreen(),
     );
   }
 }
